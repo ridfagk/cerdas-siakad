4f92682b4ab4ebd1c4d18aa2fba50c70152c28cd
ridfagk <ridwan.gotze@gmail.com>
2023-05-03 10:18:08 +0700

    add file

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
--- frontend/views/layouts/sidebar.php
+++ frontend/views/layouts/sidebar.php
@@ -13,7 +13,7 @@ use yii\helpers\Html;
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <?= Html::img('@imageurl/frontend/web/img/user.png',['class' => 'img-circle elevation-2'])?>
             </div>
             <div class="info">
                 <a href="#" class="d-block">Alexander Pierce</a>
