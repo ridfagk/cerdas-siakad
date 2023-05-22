96853b24458bdf8831c32eefaaef6e3b51329c3d
ridfagk <ridwan.gotze@gmail.com>
2023-05-22 09:28:00 +0700

    update

--- backend/views/data-prodi/index.php
+++ backend/views/data-prodi/index.php
@@ -4,8 +4,10 @@ use backend\models\DataProdi;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
+use yii\widgets\{Pjax, ListView};
 use yii\grid\GridView;
-use yii\widgets\Pjax;
+use yii\bootstrap5\LinkPager;
+
 /** @var yii\web\View $this */
 /** @var backend\models\DataProdiSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
@@ -15,41 +17,57 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="data-prodi-index">
 
-    <p>
-        <?= Html::a('Tambah Prodi', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
     <?php Pjax::begin(); ?>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+        <div class="card">
+            <div class="card-header" style="text-align:right">
+                <?= Html::a('<i class="fas fa-plus-circle"></i> Tambah Prodi', ['create'], ['class' => 'btn btn-primary btn-sm']) ?>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body table-responsive">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Kode Prodi</th>
+                        <th>Nama Prodi</th>
+                        <th>Nomor SK</th>
+                        <th>Telp Prodi</th>
+                        <th >Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?= ListView::widget([
+                                        'options' => ['class' => 'list-view'],
+                                        'dataProvider' => $dataProvider,
+                                        
+                                        'itemView' => '_prodi_item',
+                                        'pager' => [
+                                            'options'=>['class'=>'pagination justify-content-center pagination-sm','style'=>'display:none'],   // set clas name used in ui list of pagination
+                                        ],      
+                            ]) ?>
+                        
+                    </tbody>
+                </table>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer clearfix">
+                <?= 
+                    LinkPager::widget([
+                        'pagination' => $pagination,
+                        'listOptions'=>['class'=>'justify-content-center pagination pagination-sm'],
+                        'prevPageLabel' => '<i class="fas fa-angle-left"></i>',   // Set the label for the "previous" page button 
+                        'nextPageLabel' => '<i class="fas fa-angle-right"></i>',   // Set the label for the "next" page button
+                        'firstPageLabel'=>'<i class="fas fa-angle-double-left"></i>',   // Set the label for the "first" page button
+                        'lastPageLabel'=>'<i class="fas fa-angle-double-right"></i>',    // Set the label for the "last" page button
+                        'maxButtonCount'=>5,
+                        'options' => [
+                            'class' => 'ip-mosaic__pagin-list',
+                        ],
+                    ]); 
+                ?>
+                </div>
+        </div>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id_prodi',
-            'kd_prodi',
-            'nama_prodi',
-            'nomor_sk',
-            'telp_prodi',
-            //'email_prodi:email',
-            //'nama_pt',
-            //'thn_berdiri',
-            //'alamat_prodi',
-            //'akreditasi',
-            //'deskripsi',
-            //'visi',
-            //'misi',
-            //'kompetensi',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, DataProdi $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id_prodi' => $model->id_prodi]);
-                 }
-            ],
-        ],
-    ]); ?>
 
     <?php Pjax::end(); ?>
 
